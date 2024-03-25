672fc3fc0fa0bb4d7db7d801073ab3c1dc039a1d
Artiomeee-you <vasa11640@gmail.com>
2024-03-25 23:37:24 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Ваш код для маршрутов и обработки запросов здесь...
+
+app.listen(port, () => {
+    console.log(`Сервер запущен на порте ${port}`);
+});
